cf5a865ca9544e777fbd82b2e16e55b5ad842d4f
João Lopes <joaomarcoslopes@id.uff.br>
2020-09-27 14:32:09 -0300

    test: refactor into separate classes

--- src/test/java/ArraySection/ChallengeTest.java
+++ src/test/java/ArraySection/ChallengeTest.java
@@ -3,15 +3,12 @@ package ArraySection;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
-import java.util.List;
-
 import static ArraySection.sort.SortChallenge.sortIntegers;
 import static ArraySection.min.MinChallenge.*;
-import static LambdasSection.Challenges.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@DisplayName("Array Section Challenges")
 class ChallengeTest {
 
     @Test
@@ -45,46 +42,5 @@ class ChallengeTest {
         int[] result = reverse(sample);
 
         assertArrayEquals(expected, result);
-    }
-
-    @Test
-    @DisplayName("challenge3 should only return elements at odd positions from string")
-    void testChallenge3() {
-        var sample = "12345567890";
-        var expected = "24579";
-
-        var result = challenge3.apply(sample);
-
-        assertEquals(expected, result);
-    }
-
-    @Test
-    @DisplayName("challenge9 should return an ordered List")
-    void testChallenge9() {
-        List<String> sample = Arrays.asList("Amelia", "Olivia", "emily", "Isla",
-                "Ava", "oliver", "Jack", "Charlie", "harry", "Jacob");
-
-        var expected = Arrays.asList("Amelia", "Ava", "Charlie",
-                "Emily", "Harry", "Isla", "Jack", "Jacob", "Oliver", "Olivia");
-
-        var result = challenge9(sample);
-
-        assertIterableEquals(expected, result);
-    }
-
-    @Test
-    @DisplayName("challenge12 should return the number of names starting with the specified letter")
-    void testChallenge12() {
-        List<String> sample = Arrays.asList("Amelia", "Olivia", "emily", "Isla",
-                "Ava", "oliver", "Jack", "Charlie", "harry", "Jacob");
-
-        String[] letters = {"A", "B", "C", "O",};
-        int[] expected = {2, 0, 1, 2};
-
-        for (int i = 0; i < letters.length; i++) {
-            var result = challenge12(sample, letters[i]);
-            assertEquals(expected[i], result);
-        }
-
     }
 }
---
+++ src/test/java/LambdaSection/ChallengeTest.java
@@ -0,0 +1,55 @@
+package LambdaSection;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static LambdasSection.Challenges.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayName("Lambda Section Challenges")
+public class ChallengeTest {
+
+    @Test
+    @DisplayName("challenge3 should only return elements at odd positions from string")
+    void testChallenge3() {
+        var sample = "12345567890";
+        var expected = "24579";
+
+        var result = challenge3.apply(sample);
+
+        assertEquals(expected, result);
+    }
+
+    @Test
+    @DisplayName("challenge9 should return an ordered List")
+    void testChallenge9() {
+        List<String> sample = Arrays.asList("Amelia", "Olivia", "emily", "Isla",
+                "Ava", "oliver", "Jack", "Charlie", "harry", "Jacob");
+
+        var expected = Arrays.asList("Amelia", "Ava", "Charlie",
+                "Emily", "Harry", "Isla", "Jack", "Jacob", "Oliver", "Olivia");
+
+        var result = challenge9(sample);
+
+        assertIterableEquals(expected, result);
+    }
+
+    @Test
+    @DisplayName("challenge12 should return the number of names starting with the specified letter")
+    void testChallenge12() {
+        List<String> sample = Arrays.asList("Amelia", "Olivia", "emily", "Isla",
+                "Ava", "oliver", "Jack", "Charlie", "harry", "Jacob");
+
+        String[] letters = {"A", "B", "C", "O",};
+        int[] expected = {2, 0, 1, 2};
+
+        for (int i = 0; i < letters.length; i++) {
+            var result = challenge12(sample, letters[i]);
+            assertEquals(expected[i], result);
+        }
+
+    }
+}
